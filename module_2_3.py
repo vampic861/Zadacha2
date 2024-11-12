ef5517521693e26e8d74af44758bb3206fa35309
vampic861 <125185219+vampic861@users.noreply.github.com>
2024-11-12 20:22:25 +1000

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+from operator import index
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+Index_list = 0
+while 1 > 0:
+    number = index(my_list[Index_list])
+    Len = len(my_list)
+    if number < 0 or Index_list == Len:
+        break
+    else:
+        print(number)
+        Index_list = Index_list + 1
+
